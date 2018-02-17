2ba84099a0b369ae6734a5bf465716508418b4a5
Waynee <xiewayne1993@gmail.com>
2018-02-17 00:24:04 -0500

    deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,16 +20,13 @@ Myflix::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-
-
-end
-
-ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'aqueous-spire-83196.heroku.com',
   :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
+  }
+
+end
