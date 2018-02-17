e1b07fd5b54a3c0b969fc848692db903447dae7c
Waynee <xiewayne1993@gmail.com>
2018-02-17 00:17:34 -0500

    deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,11 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+
 end
 
 ActionMailer::Base.smtp_settings = {
