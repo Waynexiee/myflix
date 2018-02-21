a410d63aebf6e16177b8899f7638c7b27631618f
Waynee <xiewayne1993@gmail.com>
2018-02-21 18:47:29 -0500

    add aws s3 config

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
 on_worker_boot do
-  @sidekiq_pid ||= spawn('bundle exec sidekiq -t 5')
+  @sidekiq_pid ||= spawn('bundle exec sidekiq -t 25')
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
