0fbb45cb91e97d6c3940b8897b9327d44c553b08
Waynee <xiewayne1993@gmail.com>
2018-02-21 17:21:04 -0500

    add aws s3 config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,13 @@
 CarrierWave.configure do |config|
+  require 'carrierwave/storage/fog'
   if Rails.env.staging? || Rails.env.production?
-    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET'],
       region: ENV['S3_REGION']
     }
+    config.storage = :fog
     config.fog_directory    = ENV['S3_BUCKET_NAME']
     config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
     config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
